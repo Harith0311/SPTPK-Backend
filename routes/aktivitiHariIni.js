bb3b168fe86eec4d641d75a04fcb4986b6233db0
Harith <harithismail29@gmail.com>
2023-06-08 03:15:42 +0800

    update

--- routes/aktivitiHariIni.js
+++ routes/aktivitiHariIni.js
@@ -18,14 +18,16 @@ router.get("/aktivitiHariIni", async (req, res) => {
 router.post("/aktivitiHariIni", async (req, res) => {
     const newTodayActivity = await prisma.aktivitiHariIni.create({ data: req.body });
     res.json(newTodayActivity);
-});    
+}); 
+
+
 
 router.delete("/aktivitiHariIni/:id", async (req, res) => {
     const id = req.params.id;
     // Delete activity ID from aktivitiHariIni table
       const deletedAktivitiHariIni = await prisma.aktivitiHariIni.delete({
         where: {
-            id: id 
+            id: id  
         }
       });
       res.json(deletedAktivitiHariIni);
