a7aaff8d99927a449942cd0af345af0d4c9b051e
Harith <harithismail29@gmail.com>
2023-06-20 01:11:22 +0800

    update

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const { PrismaClient } = require("@prisma/client");
 
 app.use(express.json());
 
-app.use(cors());  
+app.use(cors({
+    origin: "*",
+}));  
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
