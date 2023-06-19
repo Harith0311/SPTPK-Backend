3131ab16f0e9f8b36209c25bc9e966fc7f0898b9
Harith <harithismail29@gmail.com>
2023-06-20 01:00:04 +0800

    update

--- routes/kekosongan.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const { PrismaClient } = require("@prisma/client");
-
-const prisma = new PrismaClient();
-
-module.exports = router;
-                                          
